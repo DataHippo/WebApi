550fe4c9c5dae4d288d059aa78743b659444f571
Daniel Cordoba <cordoba_daniel@hotmail.com>
2017-08-22 14:04:46 +0200

    pagination

--- DataHippo.Services/Entities/PageValues.cs
+++ DataHippo.Services/Entities/PageValues.cs
@@ -2,11 +2,13 @@
 
 namespace DataHippo.Services.Entities
 {
-    public class Page
+    public class PageValues
     {
         public string Next { get; set; }
         public string Previous { get; set; }
         public string First { get; set; }
         public string Last { get; set; }
+        public int TotalPages { get; set; }
+        public int TotalElements { get; set; }
     }
 }
